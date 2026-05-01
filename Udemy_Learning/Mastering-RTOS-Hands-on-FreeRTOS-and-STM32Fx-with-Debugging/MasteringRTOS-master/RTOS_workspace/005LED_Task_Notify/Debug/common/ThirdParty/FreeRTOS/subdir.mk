9c286e07226315986243723bc5e5209c0ead82f9
Kartheek Ikkurthi <kartheekikkurthi1998@gmail.com>
2026-05-02 04:10:22 +0900

    Add the Udemy Learning Certificates

---
+++ Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/005LED_Task_Notify/Debug/common/ThirdParty/FreeRTOS/subdir.mk
@@ -0,0 +1,57 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/Users/149195/OneDrive\ -\ Arrow\ Electronics,\ Inc/Documents/Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/common/ThirdParty/FreeRTOS/croutine.c \
+C:/Users/149195/OneDrive\ -\ Arrow\ Electronics,\ Inc/Documents/Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/common/ThirdParty/FreeRTOS/event_groups.c \
+C:/Users/149195/OneDrive\ -\ Arrow\ Electronics,\ Inc/Documents/Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/common/ThirdParty/FreeRTOS/list.c \
+C:/Users/149195/OneDrive\ -\ Arrow\ Electronics,\ Inc/Documents/Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/common/ThirdParty/FreeRTOS/queue.c \
+C:/Users/149195/OneDrive\ -\ Arrow\ Electronics,\ Inc/Documents/Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/common/ThirdParty/FreeRTOS/stream_buffer.c \
+C:/Users/149195/OneDrive\ -\ Arrow\ Electronics,\ Inc/Documents/Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/common/ThirdParty/FreeRTOS/tasks.c \
+C:/Users/149195/OneDrive\ -\ Arrow\ Electronics,\ Inc/Documents/Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/common/ThirdParty/FreeRTOS/timers.c 
+
+OBJS += \
+./common/ThirdParty/FreeRTOS/croutine.o \
+./common/ThirdParty/FreeRTOS/event_groups.o \
+./common/ThirdParty/FreeRTOS/list.o \
+./common/ThirdParty/FreeRTOS/queue.o \
+./common/ThirdParty/FreeRTOS/stream_buffer.o \
+./common/ThirdParty/FreeRTOS/tasks.o \
+./common/ThirdParty/FreeRTOS/timers.o 
+
+C_DEPS += \
+./common/ThirdParty/FreeRTOS/croutine.d \
+./common/ThirdParty/FreeRTOS/event_groups.d \
+./common/ThirdParty/FreeRTOS/list.d \
+./common/ThirdParty/FreeRTOS/queue.d \
+./common/ThirdParty/FreeRTOS/stream_buffer.d \
+./common/ThirdParty/FreeRTOS/tasks.d \
+./common/ThirdParty/FreeRTOS/timers.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+common/ThirdParty/FreeRTOS/croutine.o: C:/Users/149195/OneDrive\ -\ Arrow\ Electronics,\ Inc/Documents/Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/common/ThirdParty/FreeRTOS/croutine.c common/ThirdParty/FreeRTOS/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F407xx -c -I../Drivers/CMSIS/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Core/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I"D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/OS" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+common/ThirdParty/FreeRTOS/event_groups.o: C:/Users/149195/OneDrive\ -\ Arrow\ Electronics,\ Inc/Documents/Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/common/ThirdParty/FreeRTOS/event_groups.c common/ThirdParty/FreeRTOS/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F407xx -c -I../Drivers/CMSIS/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Core/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I"D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/OS" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+common/ThirdParty/FreeRTOS/list.o: C:/Users/149195/OneDrive\ -\ Arrow\ Electronics,\ Inc/Documents/Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/common/ThirdParty/FreeRTOS/list.c common/ThirdParty/FreeRTOS/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F407xx -c -I../Drivers/CMSIS/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Core/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I"D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/OS" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+common/ThirdParty/FreeRTOS/queue.o: C:/Users/149195/OneDrive\ -\ Arrow\ Electronics,\ Inc/Documents/Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/common/ThirdParty/FreeRTOS/queue.c common/ThirdParty/FreeRTOS/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F407xx -c -I../Drivers/CMSIS/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Core/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I"D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/OS" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+common/ThirdParty/FreeRTOS/stream_buffer.o: C:/Users/149195/OneDrive\ -\ Arrow\ Electronics,\ Inc/Documents/Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/common/ThirdParty/FreeRTOS/stream_buffer.c common/ThirdParty/FreeRTOS/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F407xx -c -I../Drivers/CMSIS/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Core/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I"D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/OS" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+common/ThirdParty/FreeRTOS/tasks.o: C:/Users/149195/OneDrive\ -\ Arrow\ Electronics,\ Inc/Documents/Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/common/ThirdParty/FreeRTOS/tasks.c common/ThirdParty/FreeRTOS/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F407xx -c -I../Drivers/CMSIS/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Core/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I"D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/OS" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+common/ThirdParty/FreeRTOS/timers.o: C:/Users/149195/OneDrive\ -\ Arrow\ Electronics,\ Inc/Documents/Udemy_Learning/Mastering-RTOS-Hands-on-FreeRTOS-and-STM32Fx-with-Debugging/MasteringRTOS-master/RTOS_workspace/common/ThirdParty/FreeRTOS/timers.c common/ThirdParty/FreeRTOS/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F407xx -c -I../Drivers/CMSIS/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Core/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I"D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/include" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/Config" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/OS" -I"D:/Workspace/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-common-2f-ThirdParty-2f-FreeRTOS
+
+clean-common-2f-ThirdParty-2f-FreeRTOS:
+	-$(RM) ./common/ThirdParty/FreeRTOS/croutine.d ./common/ThirdParty/FreeRTOS/croutine.o ./common/ThirdParty/FreeRTOS/croutine.su ./common/ThirdParty/FreeRTOS/event_groups.d ./common/ThirdParty/FreeRTOS/event_groups.o ./common/ThirdParty/FreeRTOS/event_groups.su ./common/ThirdParty/FreeRTOS/list.d ./common/ThirdParty/FreeRTOS/list.o ./common/ThirdParty/FreeRTOS/list.su ./common/ThirdParty/FreeRTOS/queue.d ./common/ThirdParty/FreeRTOS/queue.o ./common/ThirdParty/FreeRTOS/queue.su ./common/ThirdParty/FreeRTOS/stream_buffer.d ./common/ThirdParty/FreeRTOS/stream_buffer.o ./common/ThirdParty/FreeRTOS/stream_buffer.su ./common/ThirdParty/FreeRTOS/tasks.d ./common/ThirdParty/FreeRTOS/tasks.o ./common/ThirdParty/FreeRTOS/tasks.su ./common/ThirdParty/FreeRTOS/timers.d ./common/ThirdParty/FreeRTOS/timers.o ./common/ThirdParty/FreeRTOS/timers.su
+
+.PHONY: clean-common-2f-ThirdParty-2f-FreeRTOS
+
